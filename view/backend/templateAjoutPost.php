54bd176a47516821833ba5c09a12f5918a715c2a
bezakParis <bezak@free.fr>
2020-01-13 15:42:19 +0100

    TinyMCE 01

--- view/backend/templateAjoutPost.php
+++ view/backend/templateAjoutPost.php
@@ -3,7 +3,16 @@
     <head>
         <meta charset="utf-8" />
         <title><?= $title ?></title>
-        <link href="../../public/css/style.css" rel="stylesheet" /> 
+        <link href="../../public/css/style.css" rel="stylesheet" />
+		
+		<script src="https://cdn.tiny.cloud/1/3ztlr4a9ila1dt21uqsn3rfezzi5k63pbjxhxqw4hw6qzj99/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+		<script>
+			tinymce.init({
+			  selector: 'textarea',
+			  language_url : '../../public/langs/fr_FR.js'  // site absolute URL
+			});
+		</script>
+		
     </head>
         
     <body>
